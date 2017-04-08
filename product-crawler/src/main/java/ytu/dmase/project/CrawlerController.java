6e26ee7b74f2d77f292e71e125c89a32302509a4
Muhammed Emir Özçevik <emirozcevik@gmail.com>
2017-04-09 01:09:18 +0300

    Crawl config dosyası
    Tüm crawler ayarları crawl-config.properties dosyası üzerinden
    yapılacak.

--- product-crawler/src/main/java/ytu/dmase/project/CrawlerController.java
+++ product-crawler/src/main/java/ytu/dmase/project/CrawlerController.java
@@ -1,6 +1,10 @@
 package ytu.dmase.project;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.Set;
 
 import com.google.inject.Inject;
@@ -11,12 +15,12 @@ import edu.uci.ics.crawler4j.fetcher.PageFetcher;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 import ytu.dmase.project.crawler.ProductCrawler;
+import ytu.dmase.project.ioc.modules.RepositoryModule;
 
 public class CrawlerController {
 	
 	private Set<ProductCrawler> _crawlers;
 	
-	private String _crawlStorageFolder = "/crawlerdata/root";
 	private CrawlConfig _config;
 	private RobotstxtConfig _robotstxtConfig;
 	private RobotstxtServer _robotstxtServer;
@@ -25,12 +29,8 @@ public class CrawlerController {
 	@Inject
 	public CrawlerController(Set<ProductCrawler> crawlers) throws Exception
 	{
-		_config = new CrawlConfig();
-		_config.setCrawlStorageFolder(_crawlStorageFolder);
-		_config.setPolitenessDelay(1000);
-		
+		_config = readConfig("/crawl-config.properties");
 		_pageFetcher = new PageFetcher(_config);
-		
 		_robotstxtConfig = new RobotstxtConfig();
 		_robotstxtServer = new RobotstxtServer(_robotstxtConfig, _pageFetcher);
 		_crawlers = crawlers;
@@ -51,5 +51,59 @@ public class CrawlerController {
 		{
 			controller.waitUntilFinish();
 		}
+	}
+	
+	private CrawlConfig readConfig(String path)
+	{
+		CrawlConfig config = new CrawlConfig();
+		InputStream is = RepositoryModule.class.getResourceAsStream(path);
+		Properties prop = new Properties();
+		
+		try
+		{
+			prop.load(is);
+			
+			config.setCrawlStorageFolder(prop.getProperty("CRAWL_STORAGE"));
+			config.setResumableCrawling(prop.get("RESUMABLE").equals("true"));
+			config.setMaxDepthOfCrawling(Integer.parseInt(prop.getProperty("MAX_DEPTH")));
+			config.setMaxPagesToFetch(Integer.parseInt(prop.getProperty("MAX_PAGES")));
+			config.setPolitenessDelay(Integer.parseInt(prop.getProperty("POLITENESS_DELAY")));
+			config.setIncludeHttpsPages(prop.get("INCLUDE_HTTPS").equals("true"));
+			config.setIncludeBinaryContentInCrawling(prop.get("INCLUDE_BINARY_CONTENT").equals("true"));
+			config.setProcessBinaryContentInCrawling(prop.get("PROCESS_BINARY_CONTENT").equals("true"));
+			config.setMaxConnectionsPerHost(Integer.parseInt(prop.getProperty("MAX_CONNECTION_PER_HOST")));
+			config.setMaxTotalConnections(Integer.parseInt(prop.getProperty("MAX_TOTAL_CONNECTIONS")));
+			config.setSocketTimeout(Integer.parseInt(prop.getProperty("SOCKET_TIMEOUT")));
+			config.setConnectionTimeout(Integer.parseInt(prop.getProperty("CONNECTION_TIMEOUT")));
+			config.setMaxOutgoingLinksToFollow(Integer.parseInt(prop.getProperty("MAX_OUTGOING_LINKS")));
+			config.setMaxDownloadSize(Integer.parseInt(prop.getProperty("MAX_DOWNLOAD_SIZE")));
+			config.setFollowRedirects(prop.get("FOLLOW_REDIRECTS").equals("true"));
+			config.setOnlineTldListUpdate(prop.getProperty("ONLINE_TLD_LIST_UPDATE").equals("true"));
+			config.setShutdownOnEmptyQueue(prop.getProperty("SHUTDOWN_ON_EMPTY_QUEUE").equals("true"));
+			config.setThreadMonitoringDelaySeconds(Integer.parseInt(prop.getProperty("THREAD_MONITORING_DELAY")));
+			config.setThreadShutdownDelaySeconds(Integer.parseInt(prop.getProperty("THREAD_SHUTDOWN_DELAY")));
+			config.setCleanupDelaySeconds(Integer.parseInt(prop.getProperty("CLEANUP_DELAY")));
+			
+			String proxyHost = prop.getProperty("PROXY_HOST");
+			if(!proxyHost.equals(""))
+			{
+				config.setProxyHost(proxyHost);
+				config.setProxyUsername(prop.getProperty("PROXY_USERNAME"));
+				config.setProxyPassword(prop.getProperty("PROXY_PASSWORD"));
+				config.setProxyPort(Integer.parseInt(prop.getProperty("PROXY_PORT")));
+			}
+			
+			is.close();
+			
+			return config;
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.err.println(e.getClass().getName()+": "+e.getMessage());
+			System.exit(0);
+		}
+		
+		return null;
 	}
 }
